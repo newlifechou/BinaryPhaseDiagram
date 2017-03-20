92f77da958b648116756c18eeb21e211fbca32ff
xs.zhou <newlifechou@outlook.com>
2017-03-21 04:36:57 +0800

    show data in search view

--- BinaryPhaseDiagramTwo/Views/SearchView.xaml.cs
+++ BinaryPhaseDiagramTwo/Views/SearchView.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BinaryPhaseDiagramOperationLib;
+using System.IO;
+
+
 
 namespace BinaryPhaseDiagramTwo.Views
 {
@@ -20,9 +24,44 @@ namespace BinaryPhaseDiagramTwo.Views
     /// </summary>
     public partial class SearchView : UserControl
     {
+        private BPDOperation operate;
         public SearchView()
         {
             InitializeComponent();
+            string dir = "Images";
+            operate = new BPDOperation(dir);
+
+            GetAllBPDs();
+        }
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var elementA = txtElementA.Text.Trim();
+            var elementB = txtElementB.Text.Trim();
+            try
+            {
+                mainDg.ItemsSource = operate.GetData(elementA, elementB);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void GetAllBPDs()
+        {
+            try
+            {
+                mainDg.ItemsSource = operate.GetAllData();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private void btnAll_Click(object sender, RoutedEventArgs e)
+        {
+            GetAllBPDs();
         }
     }
 }
